c13c62be7e0fa22dd32d2b50bebd039a07dd5cb2
Judy <jyu@lcs.on.ca>
2022-02-22 12:16:00 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Judy Yu on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
